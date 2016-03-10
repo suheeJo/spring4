34d9355d2afa286af0017c4eaffd9c638b02b2ba
suheeJo <jo9172sh@gmail.com>
2016-03-10 11:18:15 +0900

    update mvcconfig

--- src/main/java/com/suhee/basic/config/MvcConfig.java
+++ src/main/java/com/suhee/basic/config/MvcConfig.java
@@ -5,12 +5,19 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @EnableWebMvc
 @Configuration
 @ComponentScan(basePackages="com.suhee.basic"/*, useDefaultFilters=false,includeFilters={@ComponentScan.Filter(Service.class)}*/)
-public class MvcConfig {
+public class MvcConfig extends WebMvcConfigurerAdapter {
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
 	
 	@Bean
 	public ViewResolver viewResolver() {
