daf1294db7d7522b6295cdc27e1e669a73390554
suheeJo <jo9172sh@gmail.com>
2016-05-11 15:18:12 +0900

    [스프링4 프로그래밍 입문] 챕터 2 - 2.1 싱글톤 객체 예제

--- src/main/java/com/suhee/basic/ch2/Main.java
+++ src/main/java/com/suhee/basic/ch2/Main.java
@@ -6,9 +6,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext("classpath:applicationContext.xml");
-		Greeter g = ctx.getBean("greeter", Greeter.class);
-		String msg = g.greet("스프링 테스트");
-		System.out.println(msg);
+		Greeter g1 = ctx.getBean("greeter", Greeter.class);
+		Greeter g2 = ctx.getBean("greeter", Greeter.class);
+		System.out.println("g1 == g2: " + (g1 == g2));
 		ctx.close();
 	}
 
