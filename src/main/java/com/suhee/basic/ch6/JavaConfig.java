9bfa5be690fb223c6b3c130c15d92c3c0db6d62d
suheeJo <jo9172sh@gmail.com>
2016-07-11 13:01:05 +0900

    [스프링4 프로그래밍 입문] 챕터 06 - 3. 객체 범위

--- src/main/java/com/suhee/basic/ch6/JavaConfig.java
+++ src/main/java/com/suhee/basic/ch6/JavaConfig.java
@@ -2,11 +2,13 @@ package com.suhee.basic.ch6;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class JavaConfig {
 
-	@Bean(initMethod = "connect", destroyMethod = "close")
+	@Bean
+	@Scope("prototype")
 	public Client2 client2() {
 		Client2 client2 = new Client2();
 		client2.setHost("서버3");
