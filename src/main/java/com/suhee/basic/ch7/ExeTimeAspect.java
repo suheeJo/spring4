38ba88dd4498c3b2859e1b97cb7e24466d120d75
suheeJo <jo9172sh@gmail.com>
2016-07-19 13:07:41 +0900

    [스프링4 프로그래밍 입문] 챕터 7 - 3.1 AOP 구현: XML 스키마 기반

---
+++ src/main/java/com/suhee/basic/ch7/ExeTimeAspect.java
@@ -0,0 +1,25 @@
+package com.suhee.basic.ch7;
+
+import java.util.Arrays;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
+
+public class ExeTimeAspect {
+	public Object measure(ProceedingJoinPoint joinPoint) throws Throwable {
+		long start = System.nanoTime();
+		
+		try {
+			Object result = joinPoint.proceed();
+			return result;
+		}finally {
+			long end = System.currentTimeMillis();
+			Signature sig = joinPoint.getSignature();
+			System.out.printf("%s.%s(%s) 실행시간 = %d\n"
+					, joinPoint.getTarget().getClass().getSimpleName()
+					, sig.getName()
+					, Arrays.toString(joinPoint.getArgs())
+					, (end - start));
+		}
+	}
+}
