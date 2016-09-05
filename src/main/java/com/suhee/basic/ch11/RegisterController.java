97c9fca3f8b6593c1ead106444ac32c6588228bd
suheeJo <jo9172sh@gmail.com>
2016-09-05 12:42:28 +0900

    12.12.1 ModelAndView 추가

--- src/main/java/com/suhee/basic/ch11/RegisterController.java
+++ src/main/java/com/suhee/basic/ch11/RegisterController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.suhee.basic.ch3.model.RegisterRequest;
 
@@ -42,5 +43,13 @@ public class RegisterController {
 		}catch(AlreadyExistingMemberException ex) {
 			return "register/step2";
 		}
+	}
+	
+	@RequestMapping(value="test", method = RequestMethod.GET)
+	public ModelAndView form() {
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("test", "data");
+		mav.setViewName("test/data");
+		return mav;
 	}
 }
